590d63dd52e5972468cb5a04584277411a275961
Chaojie Wang <cwang717@gatech.edu>
2022-01-18 23:54:03 -0500

    0142

---
+++ medium/0142.cpp
@@ -0,0 +1,35 @@
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        while (fast != NULL && fast->next != NULL) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (slow == fast) {
+                int loopSize = 0;
+                do {
+                    fast = fast->next->next;
+                    slow = slow->next;
+                    ++loopSize;
+                } while (slow != fast);
+                ListNode *result=head;
+                for (int i = 0; i < loopSize; ++i) {
+                    head = head->next;
+                }
+                while ((result++) != (head++)) {
+                    
+                }
+                return result;
+            }
+        }
+        
+        return NULL;
+    }
+};
